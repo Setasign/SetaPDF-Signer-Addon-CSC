185ae7ef11b0ef262ab9cd2ff8afb47d35fac6b1
Jan Slabon <jan.slabon@setasign.com>
2021-05-20 15:27:37 +0200

    Fixed wrong exception usage

--- src/Client.php
+++ src/Client.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace setasign\SetaPDF\Signer\Module\CSC;
 
-use Exception;
 use Psr\Http\Client\ClientExceptionInterface;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
