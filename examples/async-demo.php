6a0798b5b090741cb5ac59a4e22dc9c4406573f6
Maximilian Kresse <545671+MaximilianKresse@users.noreply.github.com>
2022-05-03 09:10:08 +0200

    Reordered async-demo

--- examples/async-demo.php
+++ examples/async-demo.php
@@ -29,42 +29,6 @@ $apiUri = $settings['apiUri'];
 $fileToSign = __DIR__ . '/Laboratory-Report.pdf';
 $resultPath = 'signed.pdf';
 
-$action = $_GET['action'] ?? 'unknown';
-switch ($action) {
-    case 'preview':
-        header('Content-Type: application/pdf');
-        header('Content-Disposition: inline; filename="' . basename($fileToSign, '.pdf') . '.pdf"');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-        header('Pragma: public');
-        $data = file_get_contents($fileToSign);
-        header('Content-Length: ' . strlen($data));
-        echo $data;
-        flush();
-        die();
-    case 'download':
-        $doc = $_SESSION[__FILE__]['pdf'];
-
-        header('Content-Type: application/pdf');
-        header('Content-Disposition: attachment; filename="' . $doc['name']);
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-        header('Pragma: public');
-        header('Content-Length: ' . strlen($doc['data']));
-        echo $doc['data'];
-        flush();
-        die();
-    case 'sign':
-        if (!isset($_SESSION[__FILE__]['oauth2AuthorizationUrl'])) {
-            echo 'No session data found.<hr/>If you want to restart the signature process click here: <a href="?reset=1">Restart</a>';
-            return;
-        }
-        $authorizationUrl = $_SESSION[__FILE__]['oauth2AuthorizationUrl'];
-
-        // Redirect the user to the authorization URL.
-        header('Location: ' . $authorizationUrl);
-        die();
-}
 
 // to create or update your access token you have to call generate-token.php first
 if (!isset($_SESSION['accessToken']['access_token'])) {
@@ -88,7 +52,7 @@ $oauth2Urls = $client->getOauth2Info();
 $provider = new GenericProvider([
     'clientId' => $settings['clientId'],
     'clientSecret' => $settings['clientSecret'],
-    'redirectUri' => rtrim($settings['demoUrl'], '/') . '/async-demo.php',
+    'redirectUri' => rtrim($settings['demoUrl'], '/') . '/async-demo.php?action=sign',
     'urlAuthorize' => $oauth2Urls['urlAuthorize'],
     'urlAccessToken' => $oauth2Urls['urlAccessToken'],
     'urlResourceOwnerDetails' => $oauth2Urls['urlResourceOwnerDetails'],
@@ -121,158 +85,206 @@ $certificates = array_map(function (string $certificate) {
 // the first certificate is always the signing certificate
 $certificate = array_shift($certificates);
 
-if (!isset($_GET['code'])) {
-    $signatureAlgorithmOid = $credentialInfo['key']['algo'][0];
 
-    // create a writer instance
-    $writer = new SetaPDF_Core_Writer_File($resultPath);
-    // create the document instance
-    $document = SetaPDF_Core_Document::loadByFilename($fileToSign, $writer);
-
-    // create the signer instance
-    $signer = new SetaPDF_Signer($document);
-
-    $module = new SetaPDF_Signer_Signature_Module_Pades();
-    $module->setCertificate($certificate);
-    $module->setExtraCertificates($certificates);
-
-    ['hashAlgorithm' => $hashAlgorithm, 'signAlgorithm' => $signAlgorithm] = Module::findHashAndSignAlgorithm($signatureAlgorithmOid);
-    $module->setDigest($hashAlgorithm);
-
-    // create a collector instance
-    $collector = new SetaPDF_Signer_ValidationRelatedInfo_Collector(new SetaPDF_Signer_X509_Collection($certificates));
-    // collect revocation information for this certificate
-    $vriData = $collector->getByCertificate($certificate);
-
-    foreach ($vriData->getOcspResponses() as $ocspResponse) {
-        $module->addOcspResponse($ocspResponse);
-    }
-    foreach ($vriData->getCrls() as $crl) {
-        $module->addCrl($crl);
-    }
-
-    $signer->setSignatureContentLength(20000);
-    $tmpDocument = $signer->preSign(
-        new SetaPDF_Core_Writer_File(SetaPDF_Core_Writer_TempFile::createTempPath()),
-        $module
-    );
-    if ($signAlgorithm === Digest::RSA_PSS_ALGORITHM) {
-        $signatureAlgorithmParameters = Module::fixPssPadding($this->padesModule);
-    }
-    $hashData = base64_encode(hash($hashAlgorithm, $module->getDataToSign($tmpDocument->getHashFile()), true));
-
-    $authorizationUrl = $provider->getAuthorizationUrl([
-        'scope' => 'credential',
-        'credentialID' => $credentialId,
-        'hash' => $hashData
-    ]);
-
-    $_SESSION[__FILE__] = [
-        'tmpDocument' => $tmpDocument,
-        'hashData' => $hashData,
-        'module' => $module,
-        'signAlgorithm' => $signAlgorithm,
-        'signAlgorithmOid' => $signatureAlgorithmOid,
-        'vriData' => $vriData,
-        'oauth2state' => $provider->getState(),
-        'oauth2AuthorizationUrl' => $authorizationUrl
-    ];
-
-    echo '<iframe src="?action=preview" style="width: 90%; height: 90%;"></iframe><br/><br/>'
-       . '<div style="text-align: right;"><a href="?action=sign" style="background-color: #4CAF50; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; border-radius: 8px;">Sign</a></div>';
-} else {
-    if (!isset($_SESSION[__FILE__]['hashData'])) {
-        echo 'No session data found.<hr/>If you want to restart the signature process click here: <a href="?reset=1">Restart</a>';
-        return;
-    }
-
-    // Check given state against previously stored one to mitigate CSRF attacks and replay attacks
-    if ($_GET['state'] !== $_SESSION[__FILE__]['oauth2state']) {
-        echo 'Invalid state<hr/>If you want to restart the signature process click here: <a href="?reset=1">Restart</a>';
-        return;
-    }
-
-    $hashData = $_SESSION[__FILE__]['hashData'];
-
-    /**
-     * @var SetaPDF_Signer_Signature_Module_Pades $module
-     */
-    $module = $_SESSION[__FILE__]['module'];
-
-    /**
-     * @var SetaPDF_Signer_TmpDocument $tmpDocument
-     */
-    $tmpDocument = $_SESSION[__FILE__]['tmpDocument'];
-
-    $sad = $provider->getAccessToken('authorization_code', [
-        'code' => $_GET['code']
-    ]);
-
-    $result = $client->signaturesSignHash(
-        $accessToken,
-        $credentialId,
-        $sad->getToken(),
-        [$hashData],
-        $_SESSION[__FILE__]['signAlgorithmOid'],
-        Digest::$oids[$module->getDigest()],
-        isset($signatureAlgorithmParameters) ? (string)$signatureAlgorithmParameters : null
-    );
-//    var_dump($result);
-    $signatureValue = (string) \base64_decode($result['signatures'][0]);
-    if ($_SESSION[__FILE__]['signAlgorithm'] === Digest::ECDSA_ALGORITHM) {
-        $signatureValue = Module::fixEccSignatures($signatureValue);
-    }
-
-    $module->setSignatureValue($signatureValue);
-
-    // get the CMS structur from the signature module
-    $cms = (string) $module->getCms();
-
-    $reader = new SetaPDF_Core_Reader_File($fileToSign);
-    $tmpWriter = new SetaPDF_Core_Writer_TempFile();
-
-    $document = SetaPDF_Core_Document::load($reader, $tmpWriter);
-    $signer = new SetaPDF_Signer($document);
-
-    $field = $signer->getSignatureField();
-    $fieldName = $field->getQualifiedName();
-    $signer->setSignatureFieldName($fieldName);
-
-    $signer->saveSignature($tmpDocument, $cms);
-    $document->finish();
-
-    $writer = new SetaPDF_Core_Writer_String();
-    $document = \SetaPDF_Core_Document::loadByFilename($tmpWriter->getPath(), $writer);
-
-    // create a VRI collector instance
-    $collector = new SetaPDF_Signer_ValidationRelatedInfo_Collector(new SetaPDF_Signer_X509_Collection($certificates));
-    $vriData = $collector->getByFieldName(
-        $document,
-        $fieldName,
-        SetaPDF_Signer_ValidationRelatedInfo_Collector::SOURCE_OCSP_OR_CRL,
-        null,
-        null,
-        $_SESSION[__FILE__]['vriData'] // pass the previously gathered VRI data
-    );
-    // and add it to the document.
-    $dss = new SetaPDF_Signer_DocumentSecurityStore($document);
-    $dss->addValidationRelatedInfoByFieldName(
-        $fieldName,
-        $vriData->getCrls(),
-        $vriData->getOcspResponses(),
-        $vriData->getCertificates()
-    );
-
-    // save and finish the final document
-    $document->save()->finish();
-
-    $_SESSION[__FILE__] = [
-        'pdf' => [
-            'name' => 'signed.pdf',
-            'data' => $writer->getBuffer()
-        ]
-    ];
-
-    echo 'The file was successfully signed. You can download the result <a href="?action=download" download="signed.pdf" target="_blank">here</a>.<hr/>'
-        . ' If you want to restart the signature process click here: <a href="?reset=1">Restart</a>';
+$action = $_GET['action'] ?? 'preSign';
+// if the oauth request was unsuccessful, return to preSign
+if ($action === 'sign' && $_GET['error']) {
+    $action = 'preSign';
 }
+
+switch ($action) {
+    case 'preSign':
+        $signatureAlgorithmOid = $credentialInfo['key']['algo'][0];
+
+        // create a writer instance
+        $writer = new SetaPDF_Core_Writer_File($resultPath);
+        // create the document instance
+        $document = SetaPDF_Core_Document::loadByFilename($fileToSign, $writer);
+
+        // create the signer instance
+        $signer = new SetaPDF_Signer($document);
+
+        $module = new SetaPDF_Signer_Signature_Module_Pades();
+        $module->setCertificate($certificate);
+        $module->setExtraCertificates($certificates);
+
+        ['hashAlgorithm' => $hashAlgorithm, 'signAlgorithm' => $signAlgorithm] = Module::findHashAndSignAlgorithm($signatureAlgorithmOid);
+        $module->setDigest($hashAlgorithm);
+
+        // create a collector instance
+        $collector = new SetaPDF_Signer_ValidationRelatedInfo_Collector(new SetaPDF_Signer_X509_Collection($certificates));
+        // collect revocation information for this certificate
+        $vriData = $collector->getByCertificate($certificate);
+
+        foreach ($vriData->getOcspResponses() as $ocspResponse) {
+            $module->addOcspResponse($ocspResponse);
+        }
+        foreach ($vriData->getCrls() as $crl) {
+            $module->addCrl($crl);
+        }
+
+        $signer->setSignatureContentLength(20000);
+        $tmpDocument = $signer->preSign(
+            new SetaPDF_Core_Writer_File(SetaPDF_Core_Writer_TempFile::createTempPath()),
+            $module
+        );
+        if ($signAlgorithm === Digest::RSA_PSS_ALGORITHM) {
+            $signatureAlgorithmParameters = Module::fixPssPadding($this->padesModule);
+        }
+        $hashData = base64_encode(hash($hashAlgorithm, $module->getDataToSign($tmpDocument->getHashFile()), true));
+
+        $authorizationUrl = $provider->getAuthorizationUrl([
+            'scope' => 'credential',
+            'credentialID' => $credentialId,
+            'hash' => $hashData
+        ]);
+
+        $_SESSION[__FILE__] = [
+            'tmpDocument' => $tmpDocument,
+            'hashData' => $hashData,
+            'module' => $module,
+            'signAlgorithm' => $signAlgorithm,
+            'signAlgorithmOid' => $signatureAlgorithmOid,
+            'vriData' => $vriData,
+            'oauth2state' => $provider->getState(),
+            'oauth2AuthorizationUrl' => $authorizationUrl
+        ];
+
+        echo '<iframe src="?action=preview" style="width: 90%; height: 90%;"></iframe><br/><br/>'
+            . '<div style="text-align: right;"><a href="?action=oauthAuthorize" style="background-color: #4CAF50; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; border-radius: 8px;">Sign</a></div>';
+        break;
+
+    case 'preview':
+        header('Content-Type: application/pdf');
+        header('Content-Disposition: inline; filename="' . basename($fileToSign, '.pdf') . '.pdf"');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Pragma: public');
+        $data = file_get_contents($fileToSign);
+        header('Content-Length: ' . strlen($data));
+        echo $data;
+        flush();
+        break;
+
+    case 'oauthAuthorize':
+        if (!isset($_SESSION[__FILE__]['oauth2AuthorizationUrl'])) {
+            echo 'No session data found.<hr/>If you want to restart the signature process click here: <a href="?reset=1">Restart</a>';
+            return;
+        }
+        $authorizationUrl = $_SESSION[__FILE__]['oauth2AuthorizationUrl'];
+
+        // Redirect the user to the authorization URL.
+        header('Location: ' . $authorizationUrl);
+        break;
+
+    case 'sign':
+        if (!isset($_SESSION[__FILE__]['hashData'])) {
+            echo 'No session data found.<hr/>If you want to restart the signature process click here: <a href="?reset=1">Restart</a>';
+            return;
+        }
+
+        // Check given state against previously stored one to mitigate CSRF attacks and replay attacks
+        if ($_GET['state'] !== $_SESSION[__FILE__]['oauth2state']) {
+            echo 'Invalid state<hr/>If you want to restart the signature process click here: <a href="?reset=1">Restart</a>';
+            return;
+        }
+
+        $hashData = $_SESSION[__FILE__]['hashData'];
+
+        /**
+         * @var SetaPDF_Signer_Signature_Module_Pades $module
+         */
+        $module = $_SESSION[__FILE__]['module'];
+
+        /**
+         * @var SetaPDF_Signer_TmpDocument $tmpDocument
+         */
+        $tmpDocument = $_SESSION[__FILE__]['tmpDocument'];
+
+        $sad = $provider->getAccessToken('authorization_code', [
+            'code' => $_GET['code']
+        ]);
+
+        $result = $client->signaturesSignHash(
+            $accessToken,
+            $credentialId,
+            $sad->getToken(),
+            [$hashData],
+            $_SESSION[__FILE__]['signAlgorithmOid'],
+            Digest::$oids[$module->getDigest()],
+            isset($signatureAlgorithmParameters) ? (string)$signatureAlgorithmParameters : null
+        );
+//    var_dump($result);
+        $signatureValue = (string) \base64_decode($result['signatures'][0]);
+        if ($_SESSION[__FILE__]['signAlgorithm'] === Digest::ECDSA_ALGORITHM) {
+            $signatureValue = Module::fixEccSignatures($signatureValue);
+        }
+
+        $module->setSignatureValue($signatureValue);
+
+        // get the CMS structur from the signature module
+        $cms = (string) $module->getCms();
+
+        $reader = new SetaPDF_Core_Reader_File($fileToSign);
+        $tmpWriter = new SetaPDF_Core_Writer_TempFile();
+
+        $document = SetaPDF_Core_Document::load($reader, $tmpWriter);
+        $signer = new SetaPDF_Signer($document);
+
+        $field = $signer->getSignatureField();
+        $fieldName = $field->getQualifiedName();
+        $signer->setSignatureFieldName($fieldName);
+
+        $signer->saveSignature($tmpDocument, $cms);
+        $document->finish();
+
+        $writer = new SetaPDF_Core_Writer_String();
+        $document = \SetaPDF_Core_Document::loadByFilename($tmpWriter->getPath(), $writer);
+
+        // create a VRI collector instance
+        $collector = new SetaPDF_Signer_ValidationRelatedInfo_Collector(new SetaPDF_Signer_X509_Collection($certificates));
+        $vriData = $collector->getByFieldName(
+            $document,
+            $fieldName,
+            SetaPDF_Signer_ValidationRelatedInfo_Collector::SOURCE_OCSP_OR_CRL,
+            null,
+            null,
+            $_SESSION[__FILE__]['vriData'] // pass the previously gathered VRI data
+        );
+        // and add it to the document.
+        $dss = new SetaPDF_Signer_DocumentSecurityStore($document);
+        $dss->addValidationRelatedInfoByFieldName(
+            $fieldName,
+            $vriData->getCrls(),
+            $vriData->getOcspResponses(),
+            $vriData->getCertificates()
+        );
+
+        // save and finish the final document
+        $document->save()->finish();
+
+        $_SESSION[__FILE__] = [
+            'pdf' => [
+                'name' => 'signed.pdf',
+                'data' => $writer->getBuffer()
+            ]
+        ];
+
+        echo 'The file was successfully signed. You can download the result <a href="?action=download" download="signed.pdf" target="_blank">here</a>.<hr/>'
+            . ' If you want to restart the signature process click here: <a href="?reset=1">Restart</a>';
+        break;
+
+    case 'download':
+        $doc = $_SESSION[__FILE__]['pdf'];
+
+        header('Content-Type: application/pdf');
+        header('Content-Disposition: attachment; filename="' . $doc['name']);
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Pragma: public');
+        header('Content-Length: ' . strlen($doc['data']));
+        echo $doc['data'];
+        flush();
+        break;
+}
+
