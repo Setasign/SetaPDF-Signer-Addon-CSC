cb102dbc8cebdb8c9a20ed4013bd9203dc332e73
Jan Slabon <jan.slabon@setasign.com>
2023-04-18 11:09:11 +0200

    Update async-demo.php
    Added information how to change the request encoding to JSON in the OAuth provider.

--- examples/async-demo.php
+++ examples/async-demo.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use League\OAuth2\Client\OptionProvider\OptionProviderInterface;
+use League\OAuth2\Client\Provider\AbstractProvider;
 use League\OAuth2\Client\Provider\GenericProvider;
 use SetaPDF_Signer_Digest as Digest;
 use setasign\SetaPDF\Signer\Module\CSC\Client;
@@ -56,7 +58,23 @@ $provider = new GenericProvider([
     'urlAuthorize' => $oauth2Urls['urlAuthorize'],
     'urlAccessToken' => $oauth2Urls['urlAccessToken'],
     'urlResourceOwnerDetails' => $oauth2Urls['urlResourceOwnerDetails'],
-]);
+] /*
+ // If your OAuth endpoint requires a JSON document instead of a form-encoded document pass following "optionProvider"
+ // to the GenericProvider constructor:
+ , [
+    'optionProvider' => new class implements OptionProviderInterface {
+        public function getAccessTokenOptions($method, array $params)
+        {
+            $options = ['headers' => ['content-type' => 'application/json']];
+
+            if ($method === AbstractProvider::METHOD_POST) {
+                $options['body'] = json_encode($params);
+            }
+
+            return $options;
+        }
+    }
+]*/);
 
 $action = $_GET['action'] ?? 'preview';
 // if the oauth request was unsuccessful, return to preSign
